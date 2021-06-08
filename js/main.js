cf3e91d81e98d0bdb7e98b11a489315115137730
Malina <i.malinovskaya@big3.ru>
2021-06-08 12:02:38 +0300

    Доработки 2.2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const getRandomInteger = (from, to) => {
 
   if(from < 0 || to <= from){
     throw new Error('Введите число больше или равно нулю, второе число диапазона должно быть больше первого');
-  };
+  }
 
   const randomInteger = Math.random() * (to - from) + from;
 
@@ -16,7 +16,7 @@ const getRandomFloat = (from, to, symbols) => {
 
   if(from < 0 || to <= from){
     throw new Error('Введите число больше или равно нулю, второе число диапазона должно быть больше первого');
-  };
+  }
 
   const RandomFloat = (Math.random() * (to - from) + from).toFixed(symbols);
 
