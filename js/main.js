c1e5634021a5247990af7eaca63cfc0f8484e6df
Malina <i.malinovskaya@big3.ru>
2021-06-20 12:18:27 +0300

    с комментами

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-// На основе написанных в прошлом задании вспомогательных функций напишите необходимые функции для создания массива
-// из 10 сгенерированных JS-объектов. Каждый объект массива — описание похожего объявления неподалёку.
-
-// Мои действия:
-// 1. Создать объекты с разными типами характеристики для объявления - автор, описание, координаты.
-// 2. Написать функцию, которая будет добавлять в массив из 10 слотов объекты
-// создать объект объявления, в котором выписать все ключи и значения согласно ТЗ
-// обозначить, что должно быть 10 объявлений
-// генерация объекта объявления
-// добавление объекта объявленияв массив
-
-
-
 const getRandomInteger = (from, to) => {
 
   if(from < 0 || to <= from){
@@ -22,6 +9,7 @@ const getRandomInteger = (from, to) => {
   return Math.round(randomInteger);
 
 };
+
 const getRandomFloat = (from, to, symbols) => {
 
   if(from < 0 || to <= from){
@@ -33,9 +21,8 @@ const getRandomFloat = (from, to, symbols) => {
   return Number(RandomFloat);
 
 };
-const getRandomArrayElement = (elements) => {
-  return elements[_.random(0, elements.length - 1)];
-};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 const TYPE_APARTMENT = [
   'palace',
@@ -54,7 +41,6 @@ const FEATURES_APARTMENT = [
   'conditioner',
 ];
 
-//написать функцию, которая выдает случайное количество значений опций дома, не повторяясь?
 const CHECKIN_APARTMENT = [
   '12:00',
   '13:00',
@@ -67,68 +53,74 @@ const CHECKOUT_APARTMENT = [
   '14:00',
 ];
 
-//1 объект состоит из
-// author, объект — описывает автора. Содержит одно поле:
-// avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png,
-// где {{xx}} — это число от 1 до 10. Перед однозначными числами ставится 0.
-// Например, 01, 02...10. Адреса изображений не повторяются.
-const author = {
-  avatar: 'img/avatars/user{{xx}}.png',
+const PHOTOS_APARTMENT = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const getAuthor = (i) => {
+  if(i > 9) {
+    return {
+      avatar: 'img/avatars/user' + i + '.png',
+    }
+  }
+  else {
+    return {
+      avatar: 'img/avatars/user' + '0' + i + '.png',
+    }
+  }
 };
 
-// offer, объект — содержит информацию об объявлении. Состоит из полей:
-//   title, строка — заголовок предложения. Придумайте самостоятельно.
-//   address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.lat}}, {{location.lng}}.
-//   price, число — стоимость. Случайное целое положительное число.
-//   type, строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
-//   rooms, число — количество комнат. Случайное целое положительное число.
-//   guests, число — количество гостей, которое можно разместить. Случайное целое положительное число.
-//   checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-//   checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-//   features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
-//   description, строка — описание помещения. Придумайте самостоятельно.
-//   photos, массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
 const offer = {
-  title: 'Сдаются апартаменты',
-  address: 'по адресу {{location.lat}}, {{location.lng}}', //верно ли тут записала? если плюсовать к строке, то линтер жалуется.
-  price: getRandomInteger(),
-  type: getRandomArrayElement(TYPE_APARTMENT),
-  rooms: getRandomInteger(),
-  guests: getRandomInteger(),
-  checkin: '', //как понять фиксированную, нужно ли делать перебор, как определить какое значение из 3 возвращать?
-  checkout: '',
+  title: 'Сдаются апартаменты', //заголовок предложения. Придумайте самостоятельно.
+  address: 'по адресу {{location.lat}}, {{location.lng}}', //составляется из географических координат по маске.. маска это регулярка какая-то?
+  price: getRandomInteger(500, 2500), //cлучайное целое положительное число.
+  type: getRandomArrayElement(TYPE_APARTMENT),// одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
+  rooms: getRandomInteger(1,6),//Случайное целое положительное число. Ну диапазон же мы объявляем? Не может же быть 100 комнат
+  guests: getRandomInteger(), //количество гостей, которое можно разместить. Случайное целое положительное число. (?)
+  checkin: '', //одно из трёх фиксированных значений: 12:00, 13:00 или 14:00. как понять фиксированную, нужно ли делать перебор, как определить какое значение из 3 возвращать?
+  checkout: '', //одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
   features: [], //какая-то функция, которая выдает случайное количество значений опций дома, не повторяясь,
   description: '1-комнатные апартаменты. С видом на колбасный магазин и склад кошачей еды',
   photos: ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg'],
 };
 
-// location, объект — местоположение в виде географических координат. Состоит из двух полей:
-//   lat, число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
-//   lng, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000
-const location = {
+const getLocation = () => ({
   lat: getRandomFloat(35.65000, 35.70000,5),
   lng: getRandomFloat(139.70000,139.80000,5),
-};
+});
 
-const createAdvert = () => {
+const getRandomArrayElements = (elements) => elements.slice(0,getRandomInteger(0,elements.length - 1));
+
+// eslint-disable-next-line id-length
+const createAdvert = (i) => {
+  const currentLocation = getLocation();
   return {
-    author: author.avatar,
-    title: offer.title,
-    address: offer.address,
-    price: offer.price,
-    type: offer.type,
-    rooms: offer.rooms,
-    guests: offer.guests,
-    checkin: offer.checkin,
-    checkout: offer.checkout,
-    features: offer.features,
-    description: offer.description,
-    photos: offer.photos,
-    lat: location.lat,
-    lng: location.lng,
+    author: getAuthor(i),
+    offer: {
+      title: 'Сдаются апартаменты',
+      address: `${currentLocation.lat  }, ${  currentLocation.lng}`,
+      price: getRandomInteger(500, 1000000),
+      type: getRandomArrayElement(TYPE_APARTMENT),
+      rooms: getRandomInteger(1, 100),
+      guests: getRandomInteger(1, 100),
+      checkin: CHECKIN_APARTMENT[getRandomInteger(0,CHECKIN_APARTMENT.length - 1)],
+      checkout: CHECKOUT_APARTMENT[getRandomInteger(0,CHECKOUT_APARTMENT.length - 1)],
+      features: getRandomArrayElements(FEATURES_APARTMENT),
+      photos: getRandomArrayElements(PHOTOS_APARTMENT),
+    },
+    location: currentLocation,
   };
 };
 
-const arrayAdverts = [];
+// eslint-disable-next-line id-length
+const adverts = new Array(10).fill(null).map((el,i) => createAdvert(i));
 
-console.log(arrayAdverts);
+// for(let i = 0; i < 10; i++) {
+//   const currentAdvert = createAdvert(i);
+//   arrayAdverts.push(currentAdvert);
+// }
+
+// eslint-disable-next-line no-console
+console.log(adverts);
