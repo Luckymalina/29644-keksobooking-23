25bf3a8742e1a836c6d5765e7d63735ca0706474
Malina <i.malinovskaya@big3.ru>
2021-06-20 12:23:12 +0300

    убрала комменты и лишнее

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-const getRandomInteger = (from, to) => {
-
-  if(from < 0 || to <= from){
-    throw new Error('Введите число больше или равно нулю, второе число диапазона должно быть больше первого');
-  }
-
-  const randomInteger = Math.random() * (to - from) + from;
-
-  return Math.round(randomInteger);
-
-};
-
-const getRandomFloat = (from, to, symbols) => {
-
-  if(from < 0 || to <= from){
-    throw new Error('Введите число больше или равно нулю, второе число диапазона должно быть больше первого');
-  }
-
-  const RandomFloat = (Math.random() * (to - from) + from).toFixed(symbols);
-
-  return Number(RandomFloat);
-
-};
-
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
 const TYPE_APARTMENT = [
   'palace',
   'flat',
@@ -57,33 +31,45 @@ const PHOTOS_APARTMENT = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-];
+];const getRandomInteger = (from, to) => {
 
+  if(from < 0 || to <= from){
+    throw new Error('Введите число больше или равно нулю, второе число диапазона должно быть больше первого');
+  }
+
+  const randomInteger = Math.random() * (to - from) + from;
+
+  return Math.round(randomInteger);
+
+};
+
+const getRandomFloat = (from, to, symbols) => {
+
+  if(from < 0 || to <= from){
+    throw new Error('Введите число больше или равно нулю, второе число диапазона должно быть больше первого');
+  }
+
+  const RandomFloat = (Math.random() * (to - from) + from).toFixed(symbols);
+
+  return Number(RandomFloat);
+
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+// eslint-disable-next-line id-length
 const getAuthor = (i) => {
   if(i > 9) {
     return {
-      avatar: 'img/avatars/user' + i + '.png',
-    }
+      avatar: `img/avatars/user${  i  }.png`,
+    };
   }
   else {
     return {
-      avatar: 'img/avatars/user' + '0' + i + '.png',
-    }
+      // eslint-disable-next-line no-useless-concat
+      avatar: `${'img/avatars/user' + '0'}${  i  }.png`,
+    };
   }
-};
-
-const offer = {
-  title: 'Сдаются апартаменты', //заголовок предложения. Придумайте самостоятельно.
-  address: 'по адресу {{location.lat}}, {{location.lng}}', //составляется из географических координат по маске.. маска это регулярка какая-то?
-  price: getRandomInteger(500, 2500), //cлучайное целое положительное число.
-  type: getRandomArrayElement(TYPE_APARTMENT),// одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
-  rooms: getRandomInteger(1,6),//Случайное целое положительное число. Ну диапазон же мы объявляем? Не может же быть 100 комнат
-  guests: getRandomInteger(), //количество гостей, которое можно разместить. Случайное целое положительное число. (?)
-  checkin: '', //одно из трёх фиксированных значений: 12:00, 13:00 или 14:00. как понять фиксированную, нужно ли делать перебор, как определить какое значение из 3 возвращать?
-  checkout: '', //одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-  features: [], //какая-то функция, которая выдает случайное количество значений опций дома, не повторяясь,
-  description: '1-комнатные апартаменты. С видом на колбасный магазин и склад кошачей еды',
-  photos: ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg'],
 };
 
 const getLocation = () => ({
@@ -116,11 +102,6 @@ const createAdvert = (i) => {
 
 // eslint-disable-next-line id-length
 const adverts = new Array(10).fill(null).map((el,i) => createAdvert(i));
-
-// for(let i = 0; i < 10; i++) {
-//   const currentAdvert = createAdvert(i);
-//   arrayAdverts.push(currentAdvert);
-// }
 
 // eslint-disable-next-line no-console
 console.log(adverts);
